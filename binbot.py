530fdaca3d210b48e49a76e2901ddd76acbff3b3
Koalacards <44925247+Koalacards@users.noreply.github.com>
2020-06-07 22:39:13 -0400

    made it so that manually assigning roles doesnt screw it up

--- binbot.py
+++ binbot.py
@@ -9,6 +9,7 @@ thumbsUp = '👍'
 thumbsDown = '👎'
 
 binMessages = []
+unbinMessages=[]
 
 numVotes = 3
 
@@ -27,20 +28,31 @@ async def on_reaction_add(reaction, user):
     guild = message.guild
     if message.id in binMessages:
         if reaction.count >= numVotes and str(reaction.emoji) == thumbsUp:
-            print('time to assign the binned role')
             toBeBinned = message.mentions[0]
-            binned = await binUnbin(guild, toBeBinned)
-            print('got past role assign')
+            binned = await binAction(guild, toBeBinned)
             binMessages.remove(message.id)
             if binned == True:
-                await message.channel.send(f'{toBeBinned.mention} has been unbinned!')
-            else:
                 await message.channel.send(f'{toBeBinned.mention} has been binned!')
                 if len(message.mentions) == 1:
                     await message.channel.send('https://giphy.com/gifs/LJemLPJs6dBhm')
+            else:
+                await message.channel.send(f'{toBeBinned.mention} was supposed to be binned, but they are already binned!')
         if reaction.count >= numVotes and str(reaction.emoji) == thumbsDown:
             await message.delete()
             binMessages.remove(message.id)
+    if message.id in unbinMessages:
+        if reaction.count >= numVotes and str(reaction.emoji) == thumbsUp:
+            toBeUnBinned = message.mentions[0]
+            unbinned = await unbinAction(guild, toBeUnBinned)
+            unbinMessages.remove(message.id)
+            if unbinned == True:
+                await message.channel.send(f'{toBeBinned.mention} has been unbinned!')
+            else:
+                await message.channel.send(f'{toBeBinned.mention} was supposed to be unbinned, but they are already unbinned!')
+        if reaction.count >= numVotes and str(reaction.emoji) == thumbsDown:
+            await message.delete()
+            unbinMessages.remove(message.id)
+    
 
 @client.command()
 async def bin(ctx, member:discord.Member, *, reason:str = ''):
@@ -67,7 +79,7 @@ async def unbin(ctx, member:discord.Member, *, reason:str = ''):
         await ctx.send(f'{member.mention} is already out of the bin!')
     else:
         message = await ctx.send(f'{member.mention} is being voted out of the bin by {ctx.message.author.mention}{reasonMessage}. React with a {thumbsUp} to vote for unbinning. {numVotes} {thumbsUp}\'s are needed.')
-        binMessages.append(message.id)
+        unbinMessages.append(message.id)
 
 
 @client.command()
@@ -83,7 +95,8 @@ async def votenum(ctx, number:int):
             await ctx.send('The vote number must be a positive integer!')
 
         
-async def binUnbin(guild, member:discord.Member):
+async def binAction(guild, member:discord.Member):
+    ''' puts someone in the bin, returns False if they already had the binned role'''
     binnedRole = await getBinnedRole(guild)
     hasBinnedRole = False
     for role in member.roles:
@@ -91,10 +104,23 @@ async def binUnbin(guild, member:discord.Member):
             hasBinnedRole = True
             break
     if hasBinnedRole == True:
-        await member.remove_roles(binnedRole)
-        return True
+        return False
     else:
         await member.add_roles(binnedRole)
+        return True
+
+async def unbinAction(guild, member:discord.Member):
+    '''Takes someone out of the bin, returns False if they are already out of the bin'''
+    binnedRole = await getBinnedRole(guild)
+    hasBinnedRole = False
+    for role in member.roles:
+        if role == binnedRole:
+            hasBinnedRole = True
+            break
+    if hasBinnedRole == True:
+        await member.add_roles(binnedRole)
+        return True
+    else:
         return False
 
 
